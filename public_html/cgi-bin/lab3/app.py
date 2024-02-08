e102dc46ebf5b28413f34dd0749b5190c69764c8
ziller <darren@counihan.ie>
2024-02-08 17:52:04 +0000

    post lab session

--- public_html/cgi-bin/lab3/app.py
+++ public_html/cgi-bin/lab3/app.py
@@ -1,7 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from forms import shift_form, conversion_form
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "this-is-my-secret-key"
+    
+@app.route("/shift", methods=["GET", "POST"])
+def shift():
+    form = shift_form()
+    plaintext = form.plainText.data
+    shift = form.shift.data
 
-@app.route("/rsp/<player>")
-def rsp(player):
-    return "Under Development!!!!!!!!!!!!!!!!!!!!!!"
+    cipherText = ""
+    if form.validate_on_submit():
+
+        for char in plaintext:
+            if char.isupper():
+                cipherText += chr((ord(char) - 65 + shift) % 26 + 65)
+            elif char.islower():
+                cipherText += chr((ord(char) - 97 + shift) % 26 + 97)
+            else:
+                cipherText += char
+        form.cipherText.data = cipherText
+        return render_template("shift_form.html", form=form )
+
+
+        
+    return render_template("shift_form.html", form=form)
+
+    
+@app.route("/conversion", methods=["GET", "POST"])
+def conversion():
+    form = conversion_form()
+    return render_template("conversion_form.html", form=form)
---
+++ public_html/cgi-bin/lab3/forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, StringField, SelectField, RadioField, IntegerField, DecimalField
+from wtforms.validators import InputRequired
+
+class shift_form(FlaskForm):
+
+    submit = SubmitField("Submit")
+    plainText = StringField("Plaintext:", validators=[InputRequired()])
+    shift = IntegerField('Shift', validators=[InputRequired()])
+    cipherText = StringField('Ciphertext:')
+
+
+class conversion_form(FlaskForm):
+    base = RadioField(choices=['Fahrenheit', 'Celsius', 'Kelvin'], default='Fahrenheit')
+    baseValue = DecimalField(validators=[InputRequired()])
+
+    output = RadioField(choices=['Fahrenheit', 'Celsius', 'Kelvin'], default='Fahrenheit')
+    outValue = DecimalField()
+  
+    
