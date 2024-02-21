ad1b21afb8c34bf247188b9b7a15421cb903a3bc
ziller <143824815+godziller@users.noreply.github.com>
2024-02-21 16:00:57 +0000

    route functions todo

---
+++ public_html/cgi-bin/ca1/app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template
+
+
+
+
+'''
+This section are all useer login route functions
+'''
+
+# @app.route("/register", methods=["GET", "POST"]) TODO:
+# def register():
+
+# @app.route("/login", methods=["GET", "POST"]) TODO:
+# def login():
+
+# @app.route("/logout", methods=["GET", "POST"]) TODO:
+# def logout():
+
+# @app.route("/unsubscribe", methods=["GET", "POST"]) TODO:
+# def unsubscribe():
+
+
+'''
+This section are all task route functions
+'''
+
+# @app.route("/list_tasks", methods=["GET", "POST"]) TODO:
+# def list_tasks():
+
+# @app.route("/update_task", methods=["GET", "POST"]) TODO:
+# def update_task():
+
+# @app.route("/delete_task", methods=["GET", "POST"]) TODO:
+# def delete_task():
+
+# @app.route("/add_task", methods=["GET", "POST"]) TODO:
+# def add_task():
+
+# @app.route("/get_upcoming_tasks", methods=["GET", "POST"]) TODO:
+# def get_upcoming tasks():
+# for today, for this week, for time window?
+
+
+'''
+this section are all project route functions
+'''
+# @app.route("/add_project", methods=["GET", "POST"]) TODO:
+# def add_project():
+
+# @app.route("/delete_project", methods=["GET", "POST"]) TODO:
+# def delete_project():
+
+# @app.route("/list_projects", methods=["GET", "POST"]) TODO:
+# def list_projects():
+
+'''
+This section are all admin special functions
+'''
+
+# @app.route("/adminlogin", methods=["GET", "POST"]) TODO:
+# def adminlogin():
+
+# @app.route("/update_admin_password, methods=["GET", "POST"]) TODO:
+# def update_admin_password():
