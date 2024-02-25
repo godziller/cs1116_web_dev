fe1f1a90f320e8361bebbc40ff88c74a98df5264
ziller <darren@counihan.ie>
2024-02-25 10:59:01 +0000

    vote lab

---
+++ public_html/cgi-bin/lect12/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import RadioField, SubmitField
+
+class VoteForm(FlaskForm):
+    vote = RadioField("What is your favourite binary number?",
+        choices= ["0","1"],
+        default="0")
+    submit = SubmitField("Submit")
